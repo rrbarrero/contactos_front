28fca74ae68d8f6738db5036e00fd27071535e72
Roberto Rodríguez <roberto.rodriguez@juntaex.es>
2021-08-26 13:28:51 +0200

    Quitamos el catch pues no es necesario

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -9,22 +9,18 @@ const login = async (username: string, password: string): Promise<string> => {
     const user: User = {username: '', logedIn: false};
     const url = process.env.REACT_APP_LOGIN_URL || "not env defined";
 
-    console.log(username, password);
-
     let loginData: LoginData = {
         username: username,
         password: password,
     };
 
-    try {
-        const response = await axios.post(url, loginData)
-        user.username = username
-        user.token = response.data.token;
-        user.logedIn = true
-        localStorage.setItem('user', JSON.stringify(user));
-    } catch (err) {
-        console.error(err);
-    }
+    
+    const response = await axios.post(url, loginData);
+    user.username = username;
+    user.token = response.data.token;
+    user.logedIn = true;
+    localStorage.setItem('user', JSON.stringify(user));
+    
     return username;
 }
 
