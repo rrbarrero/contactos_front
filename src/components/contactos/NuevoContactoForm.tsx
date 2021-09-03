3c99c71be948179a01853af47ff6f5313c7652dd
Roberto <rbarrero@posteo.net>
2021-09-04 01:03:05 +0200

    Continuamos con el formulario

--- src/components/contactos/NuevoContactoForm.tsx
+++ src/components/contactos/NuevoContactoForm.tsx
@@ -15,6 +15,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Input from '@material-ui/core/Input';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 
 
@@ -25,8 +27,8 @@ const useStyles = makeStyles((theme: Theme) =>
         box: {
             paddingLeft: drawerWidth + 20,
             display: 'grid',
-            gridTemplateColumns: '45% 45%',
-            columnGap: '10px',
+            gridTemplateColumns: '2% 90% 2%',
+            columnGap: '20px',
         },
         form: {
             color: 'black',
@@ -38,6 +40,15 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         selectEmpty: {
             marginTop: theme.spacing(2),
+        },
+        submitButton: {
+            marginTop: 14,
+        },
+        inputItem: {
+            marginLeft: 30,
+        },
+        control: {
+            padding: theme.spacing(2),
         },
     }),
 );
@@ -89,10 +100,15 @@ const NuevoContactoForm = () => {
         <>
             <Dashboard></Dashboard>
             <Box className={classes.box}>
+                <div></div>
                 <Formik
                     initialValues={initialValues}
                     validationSchema={ValidationSchema}
                     onSubmit={(values, actions) => {
+                        values.fechaAlta = new Date();
+                        if (selectedTratamiento) {
+                            values.persona.tratamiento = { ...selectedTratamiento };
+                        }
                         alert(JSON.stringify(values, null, 2));
                         actions.setSubmitting(false);
                     }}
@@ -110,61 +126,110 @@ const NuevoContactoForm = () => {
                     } = props;
                     return (
                         <Form>
-                            <FormControl className={classes.formControl}>
-                                <InputLabel id="tratamiento-select-label">Tratamiento</InputLabel>
-                                <Select
-                                    labelId="tratamiento-select-label"
-                                    id="tratamiento"
-                                    input={<Input />}
-                                    value={values.persona.tratamiento.id}
-                                    onChange={(e) => handleChangeTratamiento(e.target.value)}
-                                    renderValue={renderSelected}
-                                    defaultValue={tratamientos.length > 0 ? tratamientos[0].id : 0}
-                                >
-                                    {tratamientos.map((tratamiento) =>
-                                        <MenuItem key={tratamiento.id} value={tratamiento.id}>
-                                            {tratamiento.nombre}
-                                        </MenuItem>
-                                    )}
-                                </Select>
-                            </FormControl>
-                            <TextField
-                                fullWidth
-                                id="nombre"
-                                name="persona.nombre"
-                                label="Nombre"
-                                value={values.persona.nombre}
-                                onChange={handleChange}
-                                error={touched.persona?.nombre && Boolean(errors.persona?.nombre)}
-                                helperText={touched.persona?.nombre && errors.persona?.nombre}
-                            />
-                            <TextField
-                                fullWidth
-                                id="apellidos"
-                                name="persona.apellidos"
-                                label="Apellidos"
-                                value={values.persona.apellidos}
-                                onChange={handleChange}
-                                error={touched.persona?.apellidos && Boolean(errors.persona?.apellidos)}
-                                helperText={touched.persona?.apellidos && errors.persona?.apellidos}
-                            />
-                            <TextField
-                                fullWidth
-                                id="ciudad"
-                                name="ciudad"
-                                label="Ciudad"
-                                value={values.ciudad}
-                                onChange={handleChange}
-                                error={touched.ciudad && Boolean(errors.ciudad)}
-                                helperText={touched.ciudad && errors.ciudad}
-                            />
-                            <Button color="primary" variant="contained" fullWidth type="submit">
-                                Submit
-                            </Button>
+                            <Paper className={classes.control}>
+
+                                <Grid container spacing={2}>
+                                    <Grid item md={2} xs={12}>
+                                        <InputLabel id="tratamiento-select-label">Tratamiento</InputLabel>
+                                        <Select
+                                            labelId="tratamiento-select-label"
+                                            id="tratamiento"
+                                            input={<Input />}
+                                            value={values.persona.tratamiento.id}
+                                            onChange={(e) => handleChangeTratamiento(e.target.value)}
+                                            renderValue={renderSelected}
+                                            defaultValue={tratamientos.length > 0 ? tratamientos[0].id : 0}
+                                        >
+                                            {tratamientos.map((tratamiento) =>
+                                                <MenuItem key={tratamiento.id} value={tratamiento.id}>
+                                                    {tratamiento.nombre}
+                                                </MenuItem>
+                                            )}
+                                        </Select>
+                                    </Grid>
+                                    <Grid item md={2} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="nombre"
+                                            name="persona.nombre"
+                                            label="Nombre"
+                                            value={values.persona.nombre}
+                                            onChange={handleChange}
+                                            error={touched.persona?.nombre && Boolean(errors.persona?.nombre)}
+                                            helperText={touched.persona?.nombre && errors.persona?.nombre}
+                                        />
+                                    </Grid>
+                                    <Grid item md={2} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="apellidos"
+                                            name="persona.apellidos"
+                                            label="Apellidos"
+                                            value={values.persona.apellidos}
+                                            onChange={handleChange}
+                                            error={touched.persona?.apellidos && Boolean(errors.persona?.apellidos)}
+                                            helperText={touched.persona?.apellidos && errors.persona?.apellidos}
+                                        />
+                                    </Grid>
+                                    <Grid item md={3} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="cargo"
+                                            name="cargo"
+                                            label="Cargo"
+                                            value={values.cargo}
+                                            onChange={handleChange}
+                                            error={touched.cargo && Boolean(errors.cargo)}
+                                            helperText={touched.cargo && errors.cargo}
+                                        />
+                                    </Grid>
+                                    <Grid item md={3} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="empresa"
+                                            name="empresa"
+                                            label="Empresa"
+                                            value={values.empresa}
+                                            onChange={handleChange}
+                                            error={touched.empresa && Boolean(errors.empresa)}
+                                            helperText={touched.empresa && errors.empresa}
+                                        />
+                                    </Grid>
+                                    <Grid item md={2} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="ciudad"
+                                            name="ciudad"
+                                            label="Ciudad"
+                                            value={values.ciudad}
+                                            onChange={handleChange}
+                                            error={touched.ciudad && Boolean(errors.ciudad)}
+                                            helperText={touched.ciudad && errors.ciudad}
+                                        />
+                                    </Grid>
+                                    <Grid item md={2} xs={12} className={classes.inputItem}>
+                                        <TextField
+                                            fullWidth
+                                            id="direccion"
+                                            name="direccion"
+                                            label="Direccion"
+                                            value={values.direccion}
+                                            onChange={handleChange}
+                                            error={touched.direccion && Boolean(errors.direccion)}
+                                            helperText={touched.direccion && errors.direccion}
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Button className={classes.submitButton} color="primary" variant="contained" type="submit">
+                                    Submit
+                                </Button>
+                            </Paper>
+
                         </Form>
                     );
                 }}
                 </Formik>
+                <div></div>
             </Box>
         </>
     );
