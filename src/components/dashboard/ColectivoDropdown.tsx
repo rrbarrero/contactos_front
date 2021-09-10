bbe52ef5ddef91985dd093b37f1c531416c5356f
Roberto Rodríguez <roberto.rodriguez@juntaex.es>
2021-09-10 14:16:44 +0200

    Cambiada medidas del dropDown de colectivos

--- src/components/dashboard/ColectivoDropdown.tsx
+++ src/components/dashboard/ColectivoDropdown.tsx
@@ -42,8 +42,8 @@ const ITEM_PADDING_TOP = 8;
 const MenuProps = {
     PaperProps: {
         style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
+            maxHeight: ITEM_HEIGHT * 8.5 + ITEM_PADDING_TOP,
+            width: 350,
         },
     },
 };
